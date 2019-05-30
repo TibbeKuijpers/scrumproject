d46e4827b3e152516295b6e9ebf032eaecaf23ed
Kim0516 <47321823+Kim0516@users.noreply.github.com>
2019-05-30 16:29:33 +0200

    Add files via upload
    De profielpagina

---
+++ profielpagina/profielpagina.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="profielpagina.css">
+</head>
+
+<body>
+	<div class="container-fluid">
+		<header id="header">
+			Profielpagina
+		</header>
+		<hr>
+		
+		<h1 align="center">Gegevens</h1>
+		<div id="gegevens">
+		<form>
+  <div class="form-group row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly  id="staticEmail" value="email@example.com">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Wachtwoord</label>
+    <div class="col-sm-10">
+      <input type="text" readonly  id="staticEmail" value="Wachtwoord">
+    </div>
+  </div>
+</form>
+	</div>
+	<hr>
+		<h1 align="center">Onlangs besteld</h1>
+		<div id="content">
+		<div id="filter">
+			<br><br>
+			
+			<div id="card1" style="width: 18rem;">
+  				<img src="Images/download_A20_Rectangle_446_pattern.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Lorem Ipsum</h5>
+   					<p class="card-text">Batavus<br>€500,-</p>
+  				</div>
+			</div>
+			<div id="card2" style="width: 18rem;">
+  				<img src="Images/download_A20_Rectangle_446_pattern.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Lorem Ipsum</h5>
+   					<p class="card-text">Batavus<br>€500,-</p>
+  				</div>
+			</div>
+			<div id="card3" style="width: 18rem;">
+  				<img src="Images/download_A20_Rectangle_446_pattern.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Lorem Ipsum</h5>
+   					<p class="card-text">Batavus<br>€500,-</p>
+  				</div>
+			</div>
+			<div id="card4" style="width: 18rem;">
+  				<img src="Images/download_A20_Rectangle_446_pattern.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Lorem Ipsum</h5>
+   					<p class="card-text">Batavus<br>€500,-</p>
+  				</div>
+			</div>
+			<div id="card5" style="width: 18rem;">
+  				<img src="Images/download_A20_Rectangle_446_pattern.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Lorem Ipsum</h5>
+   					<p class="card-text">Batavus<br>€500,-</p>
+  				</div>
+			</div>
+			<div id="card6" style="width: 18rem;">
+  				<img src="Images/download_A20_Rectangle_446_pattern.png" class="card-img-top" alt="...">
+  				<div class="card-body">
+    				<h5 class="card-title">Lorem Ipsum</h5>
+   					<p class="card-text">Batavus<br>€500,-</p>
+  				</div>
+			</div>
+		</div>
+			</div>
+		<div id="uitloggen">
+			<br>
+			<hr>
+			<h1>Uitloggen</h1></div>
+	</div>
+	
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
